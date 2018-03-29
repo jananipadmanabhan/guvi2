c7a3384bb6d3c4e177ec2e790c5f981aa9b81c09
jananipadmanabhan <37689804+jananipadmanabhan@users.noreply.github.com>
2018-03-29 20:09:18 +0530

    Update count.c

--- count.c
+++ count.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    long long n;
+    long n;
     int count = 0;
 
-    printf("Enter an integer: ");
-    scanf("%lld", &n);
+    printf("Enter no: ");
+    scanf("%ld", &n);
 
     while(n != 0)
     {
+       
         n /= 10;
         ++count;
     }
 
     printf("Number of digits: %d", count);
+    return 0;
 }
