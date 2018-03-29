b0cc2bea8463b00f4b233e8fe0878be5fd9fb44e
jananipadmanabhan <37689804+jananipadmanabhan@users.noreply.github.com>
2018-03-29 20:26:40 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n,k,arr[10];
+	int value,i,j;
+	int sum=0;
+	scanf("%d",&n);
+	scanf("%d",&k);
+	for(i=0;i=10;i++);
+	{
+		scanf("%d",arr[i]);
+	}
+	for(j=0;j<=k;j++)
+	{
+		sum=sum+arr[i];
+		value=sum;
+	}
+	printf("%d",value);
+	return 0;
+}
